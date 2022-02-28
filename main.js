23569a45175f67519ac7478270af765460e4605a
ji <jiyongprk@gmail.com>
2022-02-28 14:50:28 -0500

    working on javascript

---
+++ main.js
@@ -0,0 +1,33 @@
+const rndmLorem = [{
+    title: "one",
+    text: " wagwagwa"
+},
+
+{
+    title: "two",
+    text: "lknklklnkl"
+},
+
+{
+    name: "three",
+    text: " zxcvzxcvzxcv"
+},
+
+{
+    name: "four",
+    text: " lp,pl,pmplmplm"
+}];
+
+
+const pOne = document.querySelector('.one');
+const pTwo = document.querySelector('.two');
+const pThree = document.querySelector('.three');
+const pFour = document.querySelector('.four');
+
+
+const generateBtn = document.querySelector('.generate');
+
+
+
+
+
